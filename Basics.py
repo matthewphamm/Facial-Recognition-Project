e290fd3b221665d070919cdf744ee38be5c33d9b
Matthew Pham <matthewpham03@gmail.com>
2026-03-24 18:08:03 -0700

    Add basic image processing and opening image commands

--- Basics.py
+++ Basics.py
@@ -2,3 +2,8 @@ import cv2
 import numpy as np
 import face_recognition
 
+imgElon = face_recognition.load_image_file('ImagesBasic/Elon_Musk.jpg')
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Elon Musk', imgElon)
+cv2.waitKey(0)
