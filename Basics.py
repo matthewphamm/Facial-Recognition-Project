01ac53bb48a58b474075e8910d1779a1417b818a
Matthew Pham <matthewpham03@gmail.com>
2026-03-25 18:14:07 -0700

    Add command for printing rectangle on test image

--- Basics.py
+++ Basics.py
@@ -12,6 +12,11 @@ faceLocation = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon, (faceLocation[3], faceLocation[0]), (faceLocation[1], faceLocation[2]), (255, 0, 255), 2)
 
+faceLocationTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLocationTest[3], faceLocationTest[0]), (faceLocationTest[1], faceLocationTest[2]), (255, 0, 255), 2)
+
+
 cv2.imshow('Elon Musk', imgElon)
 cv2.imshow('Elon Test', imgTest)
 cv2.waitKey(0)
