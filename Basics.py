fe15724948e96fb03e4be1005a7f359bdd417ee5
Matthew Pham <matthewpham03@gmail.com>
2026-03-25 18:07:22 -0700

    Find location of face and draw rectangle of the dimensions

--- Basics.py
+++ Basics.py
@@ -5,5 +5,9 @@ import face_recognition
 imgElon = face_recognition.load_image_file('ImagesBasic/Elon_Musk.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 
+faceLocation = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon, (faceLocation[3], faceLocation[0]), (faceLocation[1], faceLocation[2]), (255, 0, 255), 2)
+
 cv2.imshow('Elon Musk', imgElon)
 cv2.waitKey(0)
