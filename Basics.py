fba8d6e90f1761e6d51573479179c47030485d7a
Matthew Pham <matthewpham03@gmail.com>
2026-03-25 18:11:33 -0700

    Add test image to compare differences in two images

--- Basics.py
+++ Basics.py
@@ -5,6 +5,9 @@ import face_recognition
 imgElon = face_recognition.load_image_file('ImagesBasic/Elon_Musk.jpg')
 imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
 
+imgTest = face_recognition.load_image_file('ImagesBasic/Elon_Test.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
 faceLocation = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon, (faceLocation[3], faceLocation[0]), (faceLocation[1], faceLocation[2]), (255, 0, 255), 2)
