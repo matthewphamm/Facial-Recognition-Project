9cb262b8a37f04168b35c7fd927b8b5db7993045
Matthew Pham <matthewpham03@gmail.com>
2026-03-25 18:21:45 -0700

    Add distance to measure accuracy of the two images

--- Basics.py
+++ Basics.py
@@ -17,7 +17,8 @@ encodeTest = face_recognition.face_encodings(imgTest)[0]
 cv2.rectangle(imgTest, (faceLocationTest[3], faceLocationTest[0]), (faceLocationTest[1], faceLocationTest[2]), (255, 0, 255), 2)
 
 results = face_recognition.compare_faces([encodeElon], encodeTest)
-print(results)
+faceDistance = face_recognition.face_distance([encodeElon], encodeTest) # lower distance = better match
+print(results, faceDistance)
 
 cv2.imshow('Elon Musk', imgElon)
 cv2.imshow('Elon Test', imgTest)
